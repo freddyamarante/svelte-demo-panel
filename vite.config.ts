6c542b9269ed70ce8825f781af4b4e392ffa8128
freddyamarante <freddyamarante14@gmail.com>
2025-07-02 20:18:39 -0400

    feat: install button

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
+import path from 'node:path';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib"),
+    },
+  },
 });
